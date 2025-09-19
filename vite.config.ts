c0e55c80f8b06df4f882e3d11f6d6ad96603b153
satyavamsiprakash-bit <satyavamsiprakash@gmail.com>
2025-09-20 01:32:12 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    // This is needed to proxy requests to the Vercel serverless function
-    // during local development.
-    proxy: {
-      '/api': 'http://localhost:3000'
-    }
-  }
+  plugins: [react()]
 })
